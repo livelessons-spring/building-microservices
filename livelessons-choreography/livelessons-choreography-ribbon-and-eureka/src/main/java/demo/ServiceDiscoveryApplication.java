389c9fbd4d3f796437a2eaf814e851fbce84dfba
celioeduardo <celioeduardo@me.com>
2016-11-16 16:32:32 -0200

    RestTemplate bean in ServiceDiscoveryApplication
    I've had to define RestTemplate bean with the annotation @LoadBalanced in ServiceDiscoveryApplication as the example here https://spring.io/guides/gs/client-side-load-balancing/

--- livelessons-choreography/livelessons-choreography-ribbon-and-eureka/src/main/java/demo/ServiceDiscoveryApplication.java
+++ livelessons-choreography/livelessons-choreography-ribbon-and-eureka/src/main/java/demo/ServiceDiscoveryApplication.java
@@ -2,14 +2,23 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class ServiceDiscoveryApplication {
-
+	
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
